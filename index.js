2aeb6cb35b1764a7577f40fd5a97f9ee178508f4
Gabriel Rufino <contato@gabrielrufino.com>
2020-01-25 11:20:06 -0300

    Command to clone all my repositories in github

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cli = require('commander')
 
 const { version } = require('./package.json')
 
+const clone = require('./commands/clone')
+
 cli
   .version(version)
+
+cli
+  .command('clone <origin>')
+  .description('Clone all my repositories in the origin specified')
+  .action(clone)
 
 cli.parse(process.argv)
