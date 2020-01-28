03baf183fb27c445ad3b3b29795d162f444e91ed
Gabriel Rufino <contato@gabrielrufino.com>
2020-01-27 21:16:00 -0300

    Standard verify pre-commit; Standard fixes

--- commands/clone.js
+++ commands/clone.js
@@ -2,24 +2,21 @@ const axios = require('axios')
 const shell = require('shelljs')
 
 const clone = async origin => {
-  switch (origin) {
-    case 'github':
-      const { data } = await axios.get('https://api.github.com/users/gabrielrufino/repos')
+  if (origin === 'github') {
+    const { data } = await axios.get('https://api.github.com/users/gabrielrufino/repos')
 
-      const urls = data.filter(repo => !repo.archived).map(repo => repo.clone_url)
+    const urls = data.filter(repo => !repo.archived).map(repo => repo.clone_url)
 
-      shell.mkdir('github')
-      shell.cd('github')
+    shell.mkdir('github')
+    shell.cd('github')
 
-      urls.forEach(url => {
-        shell.exec(`git clone ${url}`)
-      });
+    urls.forEach(url => {
+      shell.exec(`git clone ${url}`)
+    })
 
-      shell.cd('..')
-
-      break
-    default:
-      console.error('Invalid origin')
+    shell.cd('..')
+  } else {
+    console.error('Invalid origin')
   }
 }
 
