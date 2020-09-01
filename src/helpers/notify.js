d28af295e0ebb5677c83ea9bf8ce2b0055bed714
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-01 16:21:49 +0000

    Urgency level at notify helper

--- src/helpers/notify.js
+++ src/helpers/notify.js
@@ -1,7 +1,11 @@
 const { exec } = require('shelljs')
 
-const notify = ({ message }) => {
-  exec(`notify-send gr-tools "${message}"`)
+const notify = ({ message, level = 'normal' }) => {
+  if (['low', 'normal', 'critical'].includes(level)) {
+    throw new Error('Invalid level. Valid levels: low, normal and critical')
+  }
+
+  exec(`notify-send gr-tools --urgency=${level} "${message}"`)
 }
 
 module.exports = notify
