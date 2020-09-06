34422aa25e4cad7aa222a486c82ee4bc2aa0b75a
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-06 11:05:00 -0300

    Using isRequired helper at check-for-update arguments

--- src/helpers/check-for-update.js
+++ src/helpers/check-for-update.js
@@ -1,7 +1,9 @@
 const compareVersions = require('compare-versions')
 const latestVersion = require('latest-version')
 
-const checkForUpdate = async ({ name, version }) => {
+const isRequired = require('./is-required')
+
+const checkForUpdate = async ({ name = isRequired('name'), version = isRequired('version') }) => {
   const newVersion = await latestVersion(name)
 
   const updateAvailable = compareVersions(newVersion, version)
