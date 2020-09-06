67f1a75d90533137e8324689b58c81f36fe2f03b
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-06 10:35:33 -0300

    Using isRequired helper at validatePassword helper

--- src/helpers/validate-password.js
+++ src/helpers/validate-password.js
@@ -1,9 +1,11 @@
 const shell = require('shelljs')
 const util = require('util')
 
+const isRequired = require('./is-required')
+
 const exec = util.promisify(shell.exec)
 
-const validatePassword = async password => {
+const validatePassword = async (password = isRequired('password')) => {
   try {
     await exec(`echo ${password} | sudo -S ls`, { silent: true })
   } catch {
