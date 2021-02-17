99f9e0fe33fd54e6c1e2dbb17fdd7b391252aa05
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-17 12:10:19 -0300

    Moving mongodb setup to a separated file

--- src/commands/setup/index.js
+++ src/commands/setup/index.js
@@ -2,7 +2,6 @@
 
 const inquirer = require('inquirer')
 const ora = require('ora')
-const si = require('systeminformation')
 
 const execPromise = require('../../helpers/exec-promise')
 const notify = require('../../helpers/notify')
@@ -59,37 +58,7 @@ const environments = {
     }
   },
   gh: require('./gh'),
-  mongodb: {
-    title: 'MongoDB',
-    setup: async ({ logs }) => {
-      const installing = ora('Installing mongodb environment')
-      !logs && installing.start()
-
-      try {
-        verifyBin(['wget', 'echo', 'apt'])
-
-        await execPromise('sudo apt install gnupg', { silent: !logs })
-        await execPromise('wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -', { silent: !logs })
-
-        const { distro, release } = await si.osInfo()
-
-        if (distro === 'Ubuntu' && release.startsWith('18')) {
-          await execPromise('echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list', { silent: !logs })
-        } else if (distro === 'Ubuntu' && release.startsWith('20')) {
-          await execPromise('echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list', { silent: !logs })
-        }
-
-        await execPromise('sudo apt update', { silent: !logs })
-        await execPromise('sudo apt install -y mongodb-org', { silent: !logs })
-
-        !logs && installing.succeed('mongodb environment installed')
-        notify({ message: 'mongodb environment installed' })
-      } catch (error) {
-        !logs && installing.fail('mongodb environment not installed')
-        console.error(error.message)
-      }
-    }
-  },
+  mongodb: require('./mongodb'),
   mysql: require('./mysql'),
   nvm: {
     title: 'NVM - Node Version Manager',
