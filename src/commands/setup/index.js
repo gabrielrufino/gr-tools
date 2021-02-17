b3db239179fb9ebea052363fcd42b5c5b78a0202
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-17 11:16:04 -0300

    Moving gh setup to a separated file

--- src/commands/setup/index.js
+++ src/commands/setup/index.js
@@ -58,35 +58,7 @@ const environments = {
       }
     }
   },
-  gh: {
-    title: 'gh - Github CLI',
-    setup: async ({ logs }) => {
-      const installing = ora('Installing gh environment')
-
-      try {
-        verifyBin(['snap'])
-
-        const { password } = await inquirer.prompt([
-          {
-            type: 'password',
-            name: 'password',
-            message: 'User password: ',
-            validate: p => p ? true : 'Enter the password'
-          }
-        ])
-
-        !logs && installing.start()
-
-        await execPromise(`echo ${password} | sudo -S snap install --edge gh`, { silent: !logs })
-        await execPromise(`echo ${password} | sudo -S snap connect gh:ssh-keys`, { silent: !logs })
-
-        !logs && installing.succeed('gh environment installed')
-      } catch (error) {
-        !logs && installing.fail('gh environment not installed')
-        console.error(error.message)
-      }
-    }
-  },
+  gh: require('./gh'),
   mongodb: {
     title: 'MongoDB',
     setup: async ({ logs }) => {
