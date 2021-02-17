cf32d3eaf187fd89481c9cc70c812f0f1eef27f5
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-17 12:58:03 -0300

    Moving nvm setup to a separated file

---
+++ src/commands/setup/nvm.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const inquirer = require('inquirer')
+const ora = require('ora')
+
+const execPromise = require('../../helpers/exec-promise')
+const verifyBin = require('../../helpers/verify-bin')
+
+const nvm = {
+  title: 'NVM - Node Version Manager',
+  setup: async ({ logs }) => {
+    const installing = ora('Installing nvm environment')
+    !logs && installing.start()
+
+    try {
+      verifyBin(['curl', 'bash'])
+
+      const { password } = await inquirer.prompt([
+        {
+          type: 'password',
+          name: 'password',
+          message: 'User password: ',
+          validate: p => p ? true : 'Enter the password'
+        }
+      ])
+
+      await execPromise('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash', { silent: !logs })
+      await execPromise(`echo ${password} | sudo -S npm uninstall -g gr-tools`)
+      await execPromise(`echo ${password} | sudo -S apt remove nodejs npm`, { silent: !logs })
+      await execPromise(`echo ${password} | sudo -S apt autoremove`, { silent: !logs })
+      await execPromise('nvm install v14')
+      await execPromise('npm install -g gr-tools')
+
+      !logs && installing.succeed('nvm environment installed')
+    } catch (error) {
+      !logs && installing.fail('nvm environment not installed')
+      console.error(error.message)
+    }
+  }
+}
+
+module.exports = nvm
