947e8c7af9bed4219c6955662d6b92b8e4fa763b
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-18 18:54:59 -0300

    Moving zsh setup to a separated file

--- src/commands/setup/index.js
+++ src/commands/setup/index.js
@@ -82,35 +82,7 @@ const environments = {
     }
   },
   workbench: require('./workbench'),
-  zsh: {
-    title: 'ZSH - Oh My Zsh',
-    setup: async ({ logs }) => {
-      verifyBin(['apt', 'sh', 'wget', 'git'])
-
-      const { password } = await inquirer.prompt([
-        {
-          type: 'password',
-          name: 'password',
-          message: 'User password: ',
-          validate: p => p ? true : 'Enter the password'
-        }
-      ])
-
-      if (!logs) {
-        console.warn('The options --logs is enable on zsh setup')
-      }
-
-      try {
-        await execPromise('sudo apt -y install  zsh')
-        await execPromise('sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
-        await execPromise(`echo ${password} | chsh -s $(which zsh)`)
-
-        notify({ message: 'zsh environment installed' })
-      } catch {
-        notify({ message: 'zsh environment not installed' })
-      }
-    }
-  }
+  zsh: require('./zsh')
 }
 
 const setup = async (environment, { logs }) => {
---
+++ src/commands/setup/zsh.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const inquirer = require('inquirer')
+
+const execPromise = require('../../helpers/exec-promise')
+const notify = require('../../helpers/notify')
+const verifyBin = require('../../helpers/verify-bin')
+
+const zsh = {
+  title: 'ZSH - Oh My Zsh',
+  setup: async ({ logs }) => {
+    verifyBin(['apt', 'sh', 'wget', 'git'])
+
+    const { password } = await inquirer.prompt([
+      {
+        type: 'password',
+        name: 'password',
+        message: 'User password: ',
+        validate: p => p ? true : 'Enter the password'
+      }
+    ])
+
+    if (!logs) {
+      console.warn('The options --logs is enable on zsh setup')
+    }
+
+    try {
+      await execPromise('sudo apt -y install  zsh')
+      await execPromise('sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
+      await execPromise(`echo ${password} | chsh -s $(which zsh)`)
+
+      notify({ message: 'zsh environment installed' })
+    } catch {
+      notify({ message: 'zsh environment not installed' })
+    }
+  }
+}
+
+module.exports = zsh
