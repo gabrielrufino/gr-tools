bb6db5d855db9a34f984e0043a723aa2e71fd276
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-23 22:54:46 -0300

    Asking for user password on mysql setup

--- src/commands/setup/mysql.js
+++ src/commands/setup/mysql.js
@@ -2,21 +2,21 @@
 
 const ora = require('ora')
 
-const execPromise = require('../../helpers/exec-promise')
-const notify = require('../../helpers/notify')
-const verifyBin = require('../../helpers/verify-bin')
+const { execPromise, getUserPassword, notify, verifyBin } = require('../../helpers')
 
 const mysql = {
   title: 'MySQL',
   setup: async ({ logs }) => {
-    const installing = ora('Installing mysql environment')
-    !logs && installing.start()
-
     try {
       verifyBin(['apt'])
 
-      await execPromise('sudo apt update', { silent: !logs })
-      await execPromise('sudo apt install -y mysql-server', { silent: !logs })
+      const password = await getUserPassword()
+
+      const installing = ora('Installing mysql environment')
+      !logs && installing.start()
+
+      await execPromise(`echo ${password} | sudo -S apt update`, { silent: !logs })
+      await execPromise(`echo ${password} | sudo -S apt install -y mysql-server`, { silent: !logs })
 
       !logs && installing.succeed('mysql environment installed')
       notify({ message: 'mysql environment installed' })
