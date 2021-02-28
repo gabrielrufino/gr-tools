afec3e0a2907c0d15ecc7d65a1c669408da0d76f
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-27 23:55:54 -0300

    refactoring setup of zsh

--- src/commands/setup/zsh.js
+++ src/commands/setup/zsh.js
@@ -1,30 +1,19 @@
 'use strict'
 
-const inquirer = require('inquirer')
-
-const execPromise = require('../../helpers/exec-promise')
-const notify = require('../../helpers/notify')
-const verifyBin = require('../../helpers/verify-bin')
+const { execPromise, getUserPassword, notify, verifyBin } = require('../../helpers')
 
 const zsh = {
   title: 'ZSH - Oh My Zsh',
   setup: async ({ logs }) => {
-    verifyBin(['apt', 'sh', 'wget', 'git'])
-
-    const { password } = await inquirer.prompt([
-      {
-        type: 'password',
-        name: 'password',
-        message: 'User password: ',
-        validate: p => p ? true : 'Enter the password'
-      }
-    ])
-
-    if (!logs) {
-      console.warn('The options --logs is enable on zsh setup')
-    }
-
     try {
+      verifyBin(['apt', 'sh', 'wget', 'git'])
+
+      const { password } = await getUserPassword()
+
+      if (!logs) {
+        console.warn('The options --logs is enable on zsh setup')
+      }
+
       await execPromise(`echo ${password} | sudo -S apt -y install zsh`)
       await execPromise('sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
       await execPromise(`echo ${password} | chsh -s $(which zsh)`)
