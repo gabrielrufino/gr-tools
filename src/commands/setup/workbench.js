f913972ab07535bfe27153aba01c1c0f2d21aa77
Gabriel Rufino <contato@gabrielrufino.com>
2021-02-27 23:53:48 -0300

    refactoring setup of workbench

--- src/commands/setup/workbench.js
+++ src/commands/setup/workbench.js
@@ -1,27 +1,19 @@
 'use strict'
 
-const inquirer = require('inquirer')
 const ora = require('ora')
 
-const execPromise = require('../../helpers/exec-promise')
-const notify = require('../../helpers/notify')
+const { execPromise, getUserPassword, notify } = require('../../helpers')
 
 const workbench = {
   title: 'MySQL Workbench',
   setup: async ({ logs }) => {
-    const { password } = await inquirer.prompt([
-      {
-        type: 'password',
-        name: 'password',
-        message: 'User password: ',
-        validate: p => p ? true : 'Enter the password'
-      }
-    ])
-
     const installing = ora('Installing Workbench environment')
-    !logs && installing.start()
 
     try {
+      const { password } = await getUserPassword()
+
+      !logs && installing.start()
+
       await execPromise(`echo ${password} | sudo -S snap install mysql-workbench-community --candidate`, { silent: !logs })
       await execPromise(`echo ${password} | sudo -S snap connect mysql-workbench-community:password-manager-service`, { silent: !logs })
       await execPromise(`echo ${password} | sudo -S snap connect mysql-workbench-community:ssh-keys`, { silent: !logs })
