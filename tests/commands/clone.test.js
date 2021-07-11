31f4eb745cb8627f10ca7798212e791d853b451d
Gabriel Rufino <contato@gabrielrufino.com>
2021-07-10 22:48:25 -0300

    Initial e2e test for clone command

--- tests/commands/clone.test.js
+++ tests/commands/clone.test.js
@@ -2,7 +2,7 @@ const { exec } = require('shelljs')
 
 describe('Testing the clone command', () => {
   const execInContainer = command => {
-    exec(`docker container exec gr-tools ${command}`)
+    exec(`docker container exec gr-tools_container ${command}`)
   }
 
   beforeEach(() => {
@@ -10,26 +10,22 @@ describe('Testing the clone command', () => {
       `docker container run \
         --tty \
         --detach \
-        --volume ${__dirname}/../..:/home/user/gr-tools \
+        --workdir /home/user/gr-tools \
         --env TZ="America/Sao_Paulo" \
-        --name gr-tools \
-        ubuntu:20.04
+        --name gr-tools_container \
+        node:14
       `
     )
+    exec(`docker container cp ${__dirname}/../../ gr-tools_container:/home/user`)
     execInContainer('apt update && apt upgrade -y')
-    execInContainer('apt install tzdata -y')
-    execInContainer('apt install nodejs npm -y')
-    execInContainer('npm link --prefix /home/user/gr-tools')
+    execInContainer('apt install tzdata snapd -y')
+    execInContainer('ls')
+    execInContainer('npm link')
   })
 
   afterEach(() => {
-    exec('docker container stop gr-tools')
-    exec('docker container rm gr-tools')
-  })
-
-  test('Should clone the github repositories with no problems', () => {
-    execInContainer('gr-tools clone github')
-    expect(1).toBe(1)
+    exec('docker container stop gr-tools_container')
+    exec('docker container rm gr-tools_container')
   })
 
   test('Should clone the github repositories with no problems', () => {
