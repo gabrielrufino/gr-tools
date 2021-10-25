5125efbe5645d86b02f63ccf6e1c64efa37978ec
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-25 19:41:37 -0300

    More tests for clone

--- src/commands/clone/index.test.js
+++ src/commands/clone/index.test.js
@@ -1,7 +1,27 @@
+const faker = require('faker')
+
+const github = require('./github')
 const clone = require('.')
 
+jest.mock('./github')
+
 describe(__filename, () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   test('Should be a function', () => {
     expect(clone).toBeInstanceOf(Function)
+  })
+
+  test('Should call the github function with correct arguments', async () => {
+    const logs = faker.datatype.boolean()
+    const npmInstall = faker.datatype.boolean()
+    const ssh = faker.datatype.boolean()
+    const user = faker.internet.userName()
+
+    await clone('github', { logs, npmInstall, ssh, user })
+
+    expect(github).toBeCalledWith({ logs, npmInstall, ssh, user })
   })
 })
