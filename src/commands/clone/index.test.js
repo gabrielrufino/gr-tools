ec5142ffe915e0a309caff9a7bf0117c6f556ccc
Gabriel Rufino <contato@gabrielrufino.com>
2023-10-26 11:44:16 -0300

    Fixing test

--- src/commands/clone/index.test.js
+++ src/commands/clone/index.test.js
@@ -18,10 +18,9 @@ describe(__filename, () => {
     const logs = faker.datatype.boolean()
     const npmInstall = faker.datatype.boolean()
     const ssh = faker.datatype.boolean()
-    const user = faker.internet.userName()
 
-    await clone('github', { logs, npmInstall, ssh, user })
+    await clone('github', { logs, npmInstall, ssh })
 
-    expect(github).toBeCalledWith({ logs, npmInstall, ssh, user })
+    expect(github).toBeCalledWith({ logs, npmInstall, ssh })
   })
 })
