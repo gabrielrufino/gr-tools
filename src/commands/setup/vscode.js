8e0b76dea05f2ab38725d50c9ce00b46b7a7651b
Gabriel Rufino <contato@gabrielrufino.com>
2023-10-30 13:17:45 -0300

    Refactoring setup of vscode

--- src/commands/setup/vscode.js
+++ src/commands/setup/vscode.js
@@ -13,7 +13,16 @@ const vscode = {
     try {
       !logs && installing.start()
 
-      await execPromise(`echo ${password} | sudo -S snap install --classic code`, { silent: !logs })
+      await execPromise(`
+        echo ${password} | sudo -S apt-get install wget gpg -y
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+        sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+        rm -f packages.microsoft.gpg
+        sudo apt install apt-transport-https -y
+        sudo apt update
+        sudo apt install code -y
+      `)
 
       const extensionIds = [
         'davidanson.vscode-markdownlint',
